48a5c47b636637f0620a734dff45b42cb22a4a05
YORIKOMORIO <131775927+YORIKOMORIO@users.noreply.github.com>
2023-07-31 16:09:58 +0900

    第6回ポートフォリオ

--- portfolio_morio_coding/js/script.js
+++ portfolio_morio_coding/js/script.js
@@ -3,37 +3,90 @@ $(function () {
     $('.sp_btn,.sp_nav li').on('click', function () {
         $('.sp_nav').fadeToggle();
         $('.sp_btn').toggleClass('open');
-        // spボタンにopen classをつけたり外したりしたいので上記を追加
     });
-    // slick
-    $('.works_slider').slick({    // ('')内はスライダーにしたいclass名を記述
-        // ,忘れ注意
-        autoplay: true,  // 自動で画像がスライドする
-        autoplaySpeed: 3500, // 自動でスライドする時の秒数を指定
-        dots: true,  // 画像の下にあるドットを表示させる
-        arrows: false,  // デフォルトの矢印を消す
-        // スライドしているところとボタンの色を変えたいのでcssも変更する
-    });
-    // 課題６
-    $('.gallery_wrap').slick({
-        autoplay: true,  // 　自動で画像がスライドする
-        autoplaySpeed: 2000,
-        slidesToShow: 5,
-        responsive: [
-            {
-                breakpoint: 767,
-                settings: {
-                    slidesToShow: 1
-                },
-            },
-        ],
+
+    $(function () {
+        // $('.loading').addClass('is-active')
+       
+        // setTimeout(function(){
+        //     $('.loading').fadeOut();
+        // },3300);            
+        });
+        
+    $(function () { 
+     var fadeIn = $('.fade-down');
+    $(window).on('scroll', function () {
+        $(fadeIn).each(function () {
+        var offset = $(this).offset().top;
+        var scroll = $(window).scrollTop(); 
+        var windowHeight = $(window).height();
+        if (scroll > offset - windowHeight + 350) {
+        $(this).addClass("scroll-in");
+        }
+        });
+    })
     });
     $(function () {
-        $('.loading').addClass(is-active);
-        setTimeout(function(){
-            $('.loading').fedeOut();
-        },5000);            
+    var fadeIn = $('.fadeLeft');
+    $(window).on('scroll', function () {
+        $(fadeIn).each(function () {
+        var offset = $(this).offset().top;
+        var scroll = $(window).scrollTop(); 
+        var windowHeight = $(window).height();
+        if (scroll > offset - windowHeight + 350) {
+        $(this).addClass("scroll-left");
+        }
+    });
+    })
+    });
+    $(function () {
+        var fadeIn = $('.fadeTrigger');
+        $(window).on('scroll', function () {
+            $(fadeIn).each(function () {
+            var offset = $(this).offset().top;
+            var scroll = $(window).scrollTop(); 
+            var windowHeight = $(window).height();
+            if (scroll > offset - windowHeight + 350) {
+            $(this).addClass("fade-in");
+            }
         });
-    
-    
+        })
+        });
+
+    setTimeout(() => {
+        $('.loading_m').fadeIn();
+    }, 2000);
+    setTimeout(() => {
+    $('.fv_1').addClass('is-show');
+    }, 1400);
+    setTimeout(() => {
+        $('.fv_2').addClass('is-show');
+        }, 1400);
+        setTimeout(() => {
+            $('.fv_3').addClass('is-show');
+            }, 1400);
+            setTimeout(() => {
+                $('.fv_4').addClass('is-show');
+                }, 1400);
+                setTimeout(() => {
+                    $('.fv_5').addClass('is-show');
+                    }, 1400);
+                    setTimeout(() => {
+                        $('.fv_6').addClass('is-show');
+                        }, 2300);
+                        setTimeout(() => {
+                            $('.fv_7').addClass('is-show');
+                            }, 2300);
+                            setTimeout(() => {
+                                $('.fv_8').addClass('is-show');
+                                }, 2300);
+                                setTimeout(() => {
+                                    $('.fv_9').addClass('is-show');
+                                    }, 2300);
+                                    setTimeout(() => {
+                                        $('.fv_10').addClass('is-show');
+                                        }, 2300);
+                                
+                                
+      
 }); 
